066729fda7ed9d3db49cfb0615f21daab8edfa9f
Erik Hennerfors <erik.hennerfors@obscured.se>
2021-02-12 18:06:32 +0100

    Added exception

---
+++ laget.Limiter/Exceptions/LimiterExhaustedException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using laget.Limiter.Limits;
+
+namespace laget.Limiter.Exceptions
+{
+    public class LimiterExhaustedException : Exception
+    {
+        public LimiterExhaustedException()
+        {
+        }
+
+        public LimiterExhaustedException(string message)
+            : base(message)
+        {
+        }
+
+        public LimiterExhaustedException(ILimiter limiter)
+            : base($"We've ran out of requests {limiter.History.Count()} out of {limiter.RateLimit.Amount} over {limiter.RateLimit.TimeFrame} will resets at {DateTime.Now.AddMilliseconds(limiter.ResetsAt)}")
+        {
+        }
+    }
+}
