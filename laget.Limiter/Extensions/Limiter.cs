1f663c00017f722b4a2ea3a444134b081afe64e8
Erik Hennerfors <erik.hennerfors@laget.se>
2022-09-05 07:48:49 +0200

    Updated nuget(s)

---
+++ laget.Limiter/Extensions/Limiter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace laget.Limiter.Extensions
+{
+    public static class LimiterExtension
+    {
+        public static void Warning(this ILimiter limiter, Action action, double cutoff = 0.85)
+        {
+            if (limiter.History.Count() >= (Math.Ceiling(limiter.RateLimit.Amount * cutoff)))
+            {
+                action.Invoke();
+            }
+        }
+
+        public static void Exhausted(this ILimiter limiter, Action action)
+        {
+            if (limiter.IsExhausted)
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
